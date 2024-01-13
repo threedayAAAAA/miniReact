80eda3270018370afc83ed74ff5af4c6b074d9db
j <2019292338@qq.com>
2024-01-13 21:26:15 +0800

    feat: introduce vitest

---
+++ src/test/render.test.js
@@ -0,0 +1,36 @@
+import { expect, test, describe } from 'vitest'
+import React from '../core/react-dom/React'
+import {  } from '../core/react-dom/ReactDom'
+
+describe('render', () => {
+    test('createElement', () => {
+        const app = React.createElement(
+            "div", 
+            { id: 'app' },
+            'mini',
+            'react'
+        )
+        expect(app).toEqual({
+            type: 'div',
+            props: {
+                id: 'app',
+                children: [
+                    {
+                        type: "TEXT",
+                        props: {
+                            nodeValue: "mini",
+                           children: []
+                        }
+                    },
+                    {
+                        type: "TEXT",
+                        props: {
+                            nodeValue: "react",
+                           children: []
+                        }
+                    }
+                ]
+            }
+        })
+    })
+})
