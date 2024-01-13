4f31803437e01df8bf0ab4744aaf360457af9ec2
j <2019292338@qq.com>
2024-01-13 08:47:43 +0800

    feat: render v1

--- example/app.js
+++ example/app.js
@@ -1,2 +1,14 @@
+// import ReactDom from 'react-dom/client'
+// import App from './App.jsx'
+
+// ReactDom.createRoot(document.querySelector('#app')).render(<App></App>)
+
+// v1
 const container = document.querySelector('#app')
-console.log(container)
+const root = document.createElement('div')
+root.id = 'root'
+container.append(root)
+
+const textNode = document.createTextNode('')
+textNode.nodeValue = 'text node'
+root.append(textNode)
