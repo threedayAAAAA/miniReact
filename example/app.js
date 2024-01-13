7b3b9b119fc5d1419c7ce68b65d224a53266a274
j <2019292338@qq.com>
2024-01-13 08:55:02 +0800

    feat: render v2(vdom Object)

--- example/app.js
+++ example/app.js
@@ -4,11 +4,43 @@
 // ReactDom.createRoot(document.querySelector('#app')).render(<App></App>)
 
 // v1
+// const container = document.querySelector('#app')
+// const root = document.createElement('div')
+// root.id = 'root'
+// container.append(root)
+
+// const textNode = document.createTextNode('')
+// textNode.nodeValue = 'text node'
+// root.append(textNode)
+
+// v2 vdom => dom
 const container = document.querySelector('#app')
-const root = document.createElement('div')
+
+const ELEMENT_TYPE_ENUM = {
+    TEXT: 'TEXT'
+}
+
+const textVdom = {
+    type: ELEMENT_TYPE_ENUM.TEXT,
+    props: {
+        nodeValue: 'text',
+        children: []
+    }
+}
+const rootVdom = {
+   type: "div",
+   props: {
+        id: 'root',
+        children: [
+            textObj
+        ]
+   }
+}
+
+const root = document.createElement(textVdom.type)
 root.id = 'root'
 container.append(root)
 
 const textNode = document.createTextNode('')
-textNode.nodeValue = 'text node'
+textNode.nodeValue = textVdom.props.nodeValue
 root.append(textNode)
